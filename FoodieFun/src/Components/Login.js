58f03245c325704f71ed2ddce06ecab69e666377
Robert Kulp <pedalinon@gmail.com>
2019-09-24 16:59:07 -0700

    Login form almost complete. Just need to set state

--- FoodieFun/src/Components/Login.js
+++ FoodieFun/src/Components/Login.js
@@ -1,33 +1,34 @@
 import React, { useState } from 'react';
-import { withFormik, Form, Field } from 'formik';
+import { withFormik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 import axios from 'axios';
 
 import '../../src/index.css';
 
 const Form1 = (values) => {
-	const Person = {
-		name: '',
-		email: '',
-		password: ''
-	}
+	// const Person = {
+	// 	name: '',
+	// 	email: '',
+	// 	password: ''
+	// }
 
-	const [NewProfile, addNewProfile] = useState(Person);
+	// const [NewProfile, addNewProfile] = useState(Person);
 
-	const resetForm = () => {
-		addNewProfile(Person);
-	};
+	// const resetForm = () => {
+	// 	addNewProfile(Person);
+	// };
 
 	return (
 		<Form className='form'>
 			<h2>Add Your Information Below!</h2>
-			{values.errors.name && <p className='errors'>{values.errors.name}</p>}
+			{/*values.errors.name && <p className='errors'>{values.errors.name}</p>*/}
+			<ErrorMessage name='name' />
 			<Field type='text' name='name' placeholder='name' className='field'/>
-
-			{values.errors.email && <p className='errors'>{values.errors.email}</p>}
+			<ErrorMessage name='email' />
+			{/*values.errors.email && <p className='errors'>{values.errors.email}</p>*/}
 			<Field type='text' name='email' placeholder='email' className='field'/>
-
-			{values.errors.password && <p className='errors'>{values.errors.password}</p>}
+			<ErrorMessage name='possword' />
+			{/*values.errors.password && <p className='errors'>{values.errors.password}</p>*/}
 			<Field type='password' name='password' placeholder='password' className='field'/>
 
 			<button type='submit'>Submit</button>
@@ -45,21 +46,19 @@ export default withFormik({
 		}
 	},
 
-	handleSubmit: (values) => {
-		console.log(values);
-	},
-
 	validationSchema: yup.object().shape({
 		name: yup.string().required('Full name is required!'), 
 		email: yup.string().email().required('We need an email'),
 		password: yup.string().required('password required!')
 	}),
 
-	handleSubmit: (values) => {
-    // 'https://reqres.in/api/animals'
+	handleSubmit: (values, formik) => {
     axios.post('https://reqres.in/api/animals', values)
     .then((res) => {
       console.log(res)
+
+      formik.resetForm();
+
     })
     .catch((err) => {
       console.log('error:', err)
